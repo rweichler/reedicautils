155713098dc128fe51de51213b7ddb8b7dff964b
Reed Weichler <rweichler@gmail.com>
2014-02-05 12:28:09 -0800

    autoreleasepool

--- pbcopy/main.m
+++ pbcopy/main.m
@@ -1,30 +1,35 @@
 #import <UIKit/UIKit.h>
 #include <stdio.h>
 
+void UIApplicationInitialize(); //suppress warning
+
 int main(int argc, char *argv[]) {
-    UIApplicationInitialize();
+    @autoreleasepool
+    {
+        UIApplicationInitialize();
 
-    UIPasteboard *pb = [UIPasteboard generalPasteboard];
-    NSMutableString *str = [[NSMutableString alloc] init];
-    if(argc <= 1 || isatty(fileno(stdin)) == 0)
-    {
-        char buf[BUFSIZ];
-        int size;
-        while((size = fread(buf, sizeof(char), BUFSIZ, stdin)) > 0)
-            [str appendFormat:@"%s", buf];
-    }
-    else
-    {
-        int i;
-        for(i = 1; i < argc; i++)
+        UIPasteboard *pb = [UIPasteboard generalPasteboard];
+        NSMutableString *str = [[NSMutableString alloc] init];
+        if(argc <= 1 || isatty(fileno(stdin)) == 0)
         {
-            [str appendFormat:@"%s", argv[i]];
-            if(i != argc - 1) [str appendString:@" "];
+            char buf[BUFSIZ];
+            int size;
+            while((size = fread(buf, sizeof(char), BUFSIZ, stdin)) > 0)
+                [str appendFormat:@"%s", buf];
         }
+        else
+        {
+            int i;
+            for(i = 1; i < argc; i++)
+            {
+                [str appendFormat:@"%s", argv[i]];
+                if(i != argc - 1) [str appendString:@" "];
+            }
+        }
+        pb.string = str;
+
+        [str release];
+
+        return 0;
     }
-    pb.string = str;
-
-    [str release];
-
-    return 0;
 }
